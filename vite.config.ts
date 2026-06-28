aa12dba5f22256796bef027a540355182def45e4
Jorge Quiñonez <jorge.quinonez@zurich.com>
2026-06-28 16:19:45 -0500

    Add label base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/inersim/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 700,
